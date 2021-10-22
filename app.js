f2e74def820abff976f269e2b55371343a68faed
unknown <sanjeevmaharjan19@gmail.com>
2021-10-22 14:16:13 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYm9iaWVyb3NzaWUiLCJhIjoiY2t2MjI3a2NhM254cjJucXZlbHY2eGtmaiJ9.FWBet_Giz0l4cSV46VPAIg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [85.3240,27.7172],
+    zoom: 15,
+});
